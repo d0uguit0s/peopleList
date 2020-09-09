326033af695c4d29253f4f3b15ac8ffd86a794b6
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-09-09 13:44:56 -0300

    Renderizando alguns exemplos básicos

--- App.js
+++ App.js
@@ -7,10 +7,26 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
 
 export default class App extends React.Component {
+  renderList() {
+    const names = [
+      'Eddie Van Halen',
+      'Jimi Hendrix',
+      'Chimbinha',
+      'Steve Vai'
+    ];
+
+    const textElements = names.map(name => {
+      return <Text>{name}</Text>
+    });
+
+    return textElements;
+  }
+
   render() {
     return (
       <View>
         <Header title="Pessoas!" />
+        { this.renderList() }
       </View>
     );
   }
