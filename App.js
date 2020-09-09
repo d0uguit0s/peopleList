9e3388594bf109c98e07736d2512bf070fe41a26
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-09-09 13:35:39 -0300

    Estilizando o Header e refatorando para reutilização

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Header />
+        <Header title="Pessoas!" />
       </View>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 //Existem dois tipos de componentes, o Statefull e o Stateless/Funcional
 // Statefull
@@ -13,10 +13,25 @@ import { View, Text } from 'react-native'
 
 // Stateless | Functional
 const Header = (props) => (
-    <View>
-        <Text>Este é o nosso Header!</Text>
+    <View style={style.container}>
+        <Text style={style.title}>{ props.title }</Text>
     </View>
 );
+
+// StyleSheet
+// StyleSheet.create ajuda a otimizar as correções de erros no estilo do app e a performance em geral
+const style = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        backgroundColor: '#6ca2f7',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 50,
+        color: '#fff',
+    },
+});
 
 // Exportando o código para ser importado no projeto todo
 export default Header;
