d16ca1e25ac290057633ce0c37fa1893dca78152
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-09-09 14:46:47 -0300

    Implementando a API randomuser

--- App.js
+++ App.js
@@ -6,22 +6,31 @@ import { StyleSheet, Text, View } from 'react-native';
 // Todos os imports com um caminho especifico seram procurados nos seus respectivos caminhos
 import Header from './src/components/Header'
 
+import axios from 'axios';
+
 export default class App extends React.Component {
   renderList() {
-    const names = [
-      'Eddie Van Halen',
-      'Jimi Hendrix',
-      'Chimbinha',
-      'Steve Vai'
-    ];
+    // const names = [
+    //   'Eddie Van Halen',
+    //   'Jimi Hendrix',
+    //   'Chimbinha',
+    //   'Steve Vai'
+    // ];
 
-    const textElements = names.map(name => {
-      return <Text key={name}>{name}</Text>
-    });
+    // const textElements = names.map(name => {
+    //   return <Text key={name}>{name}</Text>
+    // });
 
-    // https://randomuser.me/api/?nat=br&results=5
+    // Promisses
+    axios
+      .get('https://randomuser.me/api/?nat=br&results=5')
+      .then(response => {
+        const { results } = response.data
+        const names = results.map(people => people.name.first)
+        console.log(names);
+      })
 
-    return textElements;
+    // return textElements;
   }
 
   render() {
