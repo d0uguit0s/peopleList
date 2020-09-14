5415ea882fc4a9124e2296b2beab819b6033d51b
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-09-14 13:26:03 -0300

    Adcionando o avatar das pessoas

--- src/components/PeopleListItem.js
+++ src/components/PeopleListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 // Aqui o node irá procurar o arquivo "index.js" dentro do diretório "util"
 import { capitalizeFirstLetter } from  '../util';
@@ -15,6 +15,7 @@ const PeopleListItem = props => {
     }
     return(
         <View style={styles.line}>
+            <Image style={styles.avatar} source={{ uri: person.picture.thumbnail }} />
             <Text style={styles.lineText}>
                 { `${capitalizeFirstLetter(titulo)}. ${first} ${last}` }
             </Text>
@@ -34,6 +35,11 @@ const styles = StyleSheet.create({
     lineText: {
         fontSize: 20,
         paddingLeft: 15,
+    },
+    avatar: {
+        // O aspectRatio mantém as proporções originais da imagem, ou seja, uma img de 40X40 se for aumentada para 50 de largura ficará 50X50
+        aspectRatio: 1,
+        width: 40
     },
 });
 
