ca1c7b80eada2f73d1eaa12393bbca422d815fd3
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-09-14 14:39:57 -0300

    Refatorando páginas

---
+++ src/pages/PeoplePage.js
@@ -0,0 +1,61 @@
+// import { StatusBar } from 'expo-status-bar';
+// Todos os imports sem um caminho especifico seram procurados diretamente dentro do "node_modules"
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Todos os imports com um caminho especifico seram procurados nos seus respectivos caminhos
+import Header from '../components/Header';
+import PeopleList from '../components/PeopleList';
+
+import axios from 'axios';
+
+export default class PeoplePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: []
+    };
+  }
+
+  componentDidMount() {
+    // Promises
+    /* 
+    Estados das promises:
+    resolved - Foi resolvida, ou seja, conseguiu realizar sua função
+    rejected - Foi rejeitada, ou seja, não conseguiu realizar sua função
+    pending - Está em espera, ou seja, ainda não confirmou se conseguiu realizar sua função
+
+    */
+    axios
+    .get('https://randomuser.me/api/?nat=br&results=10')
+    .then(response => {
+      const { results } = response.data;
+      this.setState({
+        people: results
+      });
+    })
+  }
+
+  // renderList() {
+  //   const names = [
+  //     'Eddie Van Halen',
+  //     'Jimi Hendrix',
+  //     'Chimbinha',
+  //     'Steve Vai'
+  //   ];
+
+  //   const textElements = names.map(name => {
+  //     return <Text key={name}>{name}</Text>
+  //   });
+  // }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas!" />
+        <PeopleList people={this.state.people} />
+      </View>
+    );
+  }
+}
