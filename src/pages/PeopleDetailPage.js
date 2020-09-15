1361be3297c75cb85d9121fc6ac4ae6c6435d21c
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-09-15 16:22:30 -0300

    Começando a página de detalhes
    Trouxe o avatar da pessoa para a página de detalhes

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default class PeopleDetailPage extends React.Component {
     render() {
+        const { person } = this.props.route.params;
+        const { first } = person.name;
+
         return (
-            <View>
-                <Text>Eu sou a paginaaaaaaaaaaaaa!!!</Text>
+            <View style={styles.container}>
+                <Image
+                style={styles.avatar}
+                source={{ uri: person.picture.large }}
+            />
+            <Text>{ first }</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 15,
+    },
+    avatar: {
+        aspectRatio: 1,
+    }
+});
